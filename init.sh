5f91c88312c052d5c3a6be4d388bc2c313f9c1a7
Fabien Di Tore <fabien@ditore.ch>
2014-08-23 13:53:15 +0200

    ok

--- init.sh
+++ init.sh
@@ -9,9 +9,27 @@ if [[ ! -f /.rtorrent_configured ]]; then
 	
 	sed "s/scgi_port = 0.0.0.0:5000/scgi_port = 0.0.0.0:$RPC_PORT/g" /root/.rtorrent.rc  > /root/.rtorrent.rc.tmp && mv /root/.rtorrent.rc.tmp /root/.rtorrent.rc && rm /root/.rtorrent.rc.tmp
 	
+
+
 	touch /.rtorrent_configured
 
 
+fi
+
+if [[ ! -d /data/session ]]; then
+	mkdir -p /data/session
+fi
+
+if [[ ! -d /data/complete ]]; then
+	mkdir -p /data/complete
+fi
+
+if [[ ! -d /data/download ]]; then
+	mkdir -p /data/download
+fi
+
+if [[ ! -d /data/auto ]]; then
+	mkdir -p /data/auto
 fi
 
 supervisord -c /etc/supervisor/supervisor.conf
